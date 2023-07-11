59cb64b70e83205c9f8ef7796f95dd5f51299f5c
Jyoti Prakash Sasmal <jyotiprakashsasmal2507@gmail.com>
2023-07-11 20:30:26 +0530

    Adding

--- LetsCode/Diff.java
+++ LetsCode/Diff.java
@@ -2,7 +2,7 @@ public class Diff {
     public static void main(String[] args) {
         int a=5;
         int b=3;
-        System.out.println(a-b);
+        System.out.println("The difference of two number is "+(a-b));
     }
     
 }
