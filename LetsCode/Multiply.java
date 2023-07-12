4ea43513b90f6ce7c8be62e2348d8f3537d08465
Jyoti Prakash Sasmal <jyotiprakashsasmal2507@gmail.com>
2023-07-12 20:26:21 +0530

    Adding new to the multiply message

--- LetsCode/Multiply.java
+++ LetsCode/Multiply.java
@@ -2,8 +2,8 @@ public class Multiply {
     public static void main(String[] args) {
         int a=5;
         int b=3;
-        System.out.println((a*b));
-        
+        System.out.println("The multipy of two number is "+(a*b));
+
     }
     
 }
